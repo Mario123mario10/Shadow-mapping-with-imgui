516f84643400abd156964d390087bae75c23af33
Blazej Ejzak <Blazej_Ejzak@bose.com>
2023-10-10 13:00:19 +0200

    added camera module

---
+++ sources/camera/camera.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <glm/glm.hpp>
+
+enum class Bindings : int {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
+
+class Camera {
+protected:
+    glm::mat4 projectionMatrix;
+    mutable glm::mat4 viewMatrix;
+    mutable glm::mat4 projViewMatrix;
+    glm::vec3 worldUp, up, front, right, position;
+    float near, far;
+public:
+    Camera(float near, float far, const glm::vec3& position);
+    virtual ~Camera() {}
+    const glm::mat4& getViewMatrix() const;
+    const glm::mat4& getProjectionMatrix() const;
+    float getNear() const;
+    float getFar() const;
+    const glm::vec3& getPosition() const;
+    const glm::vec3& getDirectionVector() const;
+    const glm::vec3& getUpVector() const;
+    void setDirection(const glm::vec3& direction);
+    void setDirectionLookAt(const glm::vec3& lookAtPosition);
+    void setPosition(const glm::vec3& newPosition);
+};
+
+class OrthographicCamera : public Camera {
+    float width, height;
+public:
+    OrthographicCamera(float near, float far, float width, float height, const glm::vec3& position);
+    virtual ~OrthographicCamera() = default;
+};
+
+class PerspectiveCamera : public Camera {
+protected:
+    float aspectRatio, fovy;
+public:
+    PerspectiveCamera(float near, float far, float aspectRatio, float fovy, const glm::vec3& position);
+    virtual ~PerspectiveCamera() = default;
+    float getAspectRatio() const;
+    float getFovy() const;
+};
+
+class FPSCamera : public PerspectiveCamera {
+    float lastx, lasty;
+    float yaw, pitch;
+    float movementSpeed;
+public:
+    FPSCamera(float near, float far, float aspectRatio, const glm::vec3& position, float fovy);
+    void updateEuler(float mousex, float mousey);
+    void updateQuat(float mousex, float mousey);
+    void processKeyboard(Bindings binding, float deltaTime);
+    void setMovementSpeed(float speed);
+    // TODO from direction vectors of camera set angles properly
+    void setDirection(const glm::vec3& direction) = delete;
+    void setDirectionLookAt(const glm::vec3& lookAtPosition) = delete;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <object_loader.h>
 #include <index_buffer.h>
 #include <primitives.h>
+#include <camera.h>
 // standard template libraries
 #include <fstream>
 #include <sstream>
@@ -19,19 +20,18 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow* window, FPSCamera& camera, float deltaTime);
+
 int main() {
-    // Initialize GLFW
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW." << std::endl;
         return -1;
     }
 
-    // Set GLFW to use OpenGL 3.3 core profile
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Create a GLFW windowed mode window and its OpenGL context
     GLFWwindow* window = glfwCreateWindow(1920, 1080, "Interpolated Triangle with Shaders", NULL, NULL);
     if (!window) {
         std::cerr << "Failed to create GLFW window." << std::endl;
@@ -39,10 +39,10 @@ int main() {
         return -1;
     }
 
-    // Make the window's context current
     glfwMakeContextCurrent(window);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glfwSwapInterval(0);
-    // Initialize GLAD to load OpenGL function pointers
+
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cerr << "Failed to initialize GLAD." << std::endl;
         return -1;
@@ -50,63 +50,76 @@ int main() {
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
-    //ObjectLoader<unsigned char> obj(MODELS_PATH "cube.obj");
-    // Set the viewport size and register framebuffer size callback
+
     int screenWidth, screenHeight;
     glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
     glViewport(0, 0, screenWidth, screenHeight);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    FPSCamera camera(0.1f, 100.0f, static_cast<float>(screenWidth) / static_cast<float>(screenHeight), glm::vec3(0.0f, 0.0f, 5.0f), glm::radians(60.0f));
     Shader shader(SHADERS_PATH "vertex.vert.glsl", SHADERS_PATH "fragment.frag.glsl");
     ObjectLoader<unsigned char> obj(MODELS_PATH "cube.obj");
 
     VertexArray vao;
     VertexBuffer vbo;
     IndexBuffer ibo;
-
-    {
-        vao.use();
-        vbo.use();
-        ibo.use();
-        ibo.parseElementData(obj.getIndices());
-        VertexBufferLayout layout = vbo.parseVertexData(obj.getVertices());
-        vao.addLayout(layout);
-    }
+    vao.use();
+    vbo.use();
+    ibo.use();
+    ibo.parseElementData(obj.getIndices());
+    VertexBufferLayout layout = vbo.parseVertexData(obj.getVertices());
+    vao.addLayout(layout);
     
     glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
-    glm::vec3 cameraPos = glm::vec3(-1.0f, 3.0f, -3.0f);
-    glm::vec3 point = glm::vec3(0.0f, 0.0f, 0.0f);
-    glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
-    glm::mat4 view = glm::lookAt(cameraPos, point, worldUp);
-    glm::mat4 projection = glm::perspective(glm::radians(60.0f), static_cast<float>(screenWidth) / static_cast<float>(screenHeight), 0.1f, 100.0f);
-    glm::mat4 PVM = projection * view * model;
+    camera.setMovementSpeed(3.0f);
     // Main loop
     float t = 0.0f;
-    float stop = 0.0f;
+    float last = 0.0f;
     while (!glfwWindowShouldClose(window)) {
-        float deltaTime = glfwGetTime() - stop;
-        stop = glfwGetTime();
-        // Poll for and process events
+        float deltaTime = glfwGetTime() - last;
+        last = glfwGetTime();
+
+        processInput(window, camera, deltaTime);
         glfwPollEvents();
 
-        // Rendering commands here
-        // Clear the color buffer
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         model = glm::rotate(glm::mat4(1.0f), t, glm::vec3(0.0f, 1.0f, 0.0f)) * glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 1.0f));
         t += deltaTime;
-        // Use the shader program and draw the triangle
+
         shader.use();
-        shader.modifyUniform<glm::mat4>("PVM", projection * view * model);
+        shader.modifyUniform<glm::mat4>("PVM", camera.getProjectionMatrix() * camera.getViewMatrix() * model);
+
         vao.use();
         glDrawElements(GL_TRIANGLES, obj.getIndices().size(), obj.getType(), 0);
-        glBindVertexArray(0);
 
         // Swap the front and back buffers
         glfwSwapBuffers(window);
     }
 
-    // Terminate GLFW
     glfwTerminate();
 
     return 0;
+}
+
+void processInput(GLFWwindow* window, FPSCamera& camera, float deltaTime)
+{
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
+    camera.updateEuler(xpos, ypos);
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.processKeyboard(Bindings::FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.processKeyboard(Bindings::BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.processKeyboard(Bindings::LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.processKeyboard(Bindings::RIGHT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+        camera.processKeyboard(Bindings::UP, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        camera.processKeyboard(Bindings::DOWN, deltaTime);
 }
